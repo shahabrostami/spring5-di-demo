0ee71c50df9efc120be46b637ce9301ed0c438ec
Shahab Rostami <shahab.rostami@blueworx.com>
2019-02-19 11:40:49 -0600

    Fixed

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,5 +1,7 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
+	@Autowired
     private GreetingRepository greetingRepository;
 
     @Override
