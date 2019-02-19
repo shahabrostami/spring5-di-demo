5c86ceb539e60a80856714fa2648382ba5d6faaf
Shahab Rostami <shahab.rostami@blueworx.com>
2019-02-19 13:07:07 -0600

    Correct updates for DI

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,7 +1,5 @@
 package guru.springframework.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -14,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
-	@Autowired
     private GreetingRepository greetingRepository;
+    
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+		this.greetingRepository = greetingRepository;
+	}
 
-    @Override
+	@Override
     public String sayGreeting() {
         return greetingRepository.getGermanGreeting();
     }
